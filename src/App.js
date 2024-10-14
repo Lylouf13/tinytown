e2538da1f8857cf381dcf0d9699b608e85c2ef8e
Lylouf13 <lylian.leutard@gmail.com>
2024-10-14 12:21:56 +0200

    project init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo512.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          For here is a town <br/>
+          For it is thy burden, <br/>
+          Thou shall guide them
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
