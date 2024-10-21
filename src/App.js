423983fe78f77d53b9f0acac37c2bbc35a808593
Lylouf13 <lylian.leutard@gmail.com>
2024-10-21 08:50:28 +0200

    Added basic unit creation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo512.png';
+import { useState } from 'react';
 import './App.css';
 
+import Button from './Components/Button';
+
 function App() {
+
+  const [humans, setHumans] = useState(0);
+  const [spearmen, setSpearmen] = useState(0);
+  const [bowers, setBowers] = useState(0);
+
+  const trainUnit = (threshold, unit) => {
+    if (humans >= threshold){
+      setHumans(humans-threshold)
+
+      switch(unit){
+        case "spearmen":
+          setSpearmen(spearmen+threshold)
+          break;
+        case "bowers":
+          setBowers(bowers+threshold)
+          break;
+      }
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +33,13 @@ function App() {
           For it is thy burden, <br/>
           Thou shall guide them
         </p>
+        <p>{humans} left to guide</p>
+        <Button label={"New Week"} function={()=>setHumans(humans+15)}/>
+        <p>{spearmen} wearing a spear</p>
+        <Button label={"Train 5 Spearmen"} function={()=>trainUnit(5, "spearmen")}/>
+        <p>{bowers} wearing a bow</p>
+        <Button label={"Train 5 Bowers"} function={()=>trainUnit(5, "bowers")}/>
+
       </header>
     </div>
   );
